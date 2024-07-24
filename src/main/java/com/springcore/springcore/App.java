913220e8c7e6b6cd5c321397ebdfb3b04da1533f
Vipin019 <vkpmail019@gmail0com>
2024-07-24 16:56:18 +0530

    Spring core example.

---
+++ src/main/java/com/springcore/springcore/App.java
@@ -0,0 +1,12 @@
+package com.springcore.springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Student student1 = (Student) context.getBean("student1");
+        System.out.println(student1); 
+    }
+}
